6f37df6e6b44e168e56eb404f8d43fac30e4b395
Luigi Ponti <quartese@gmail.com>
2026-05-19 12:12:58 +0200

    apply cosmetics to license headers

--- cassava/nitr.pas
+++ cassava/nitr.pas
@@ -1,14 +1,16 @@
-{ Authors: 
-- Andrew Paul Gutierrez (University of California, Berkeley / CASAS Global 
-	(Center for Analysis of Sustainable Agriculture Systems) 
-	<casas.kensington gmail.com>
-- Luigi Ponti (ENEA - Agenzia nazionale per le nuove tecnologie, l'energia e 
-	lo sviluppo economico sostenibile / CASAS Global) <quartese gmail.com>
+{
+  Authors:
+  - Andrew Paul Gutierrez (University of California, Berkeley / CASAS Global -
+    Center for Analysis of Sustainable Agriculture Systems) 
+    <casas.kensington@gmail.com>
+  - Luigi Ponti (ENEA - Agenzia nazionale per le nuove tecnologie, l'energia e
+    lo sviluppo economico sostenibile / CASAS Global) <quartese@gmail.com>
 
-Copyright: (C) CASAS Global (Center for the Analysis of Sustainable 
-	Agricultural Systems)
+  Copyright (C) 1999 CASAS Global (Center for the Analysis of Sustainable
+  Agricultural Systems)
 
-SPDX-License-Identifier: GPL-3.0-or-later }
+  SPDX-License-Identifier: GPL-3.0-or-later
+}
 
 Unit Nitr;
 interface
